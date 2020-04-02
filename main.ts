754b382bd8e616baff1b0e4e2d88da39c2531c41
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2020-04-01 22:52:25 -0400

    added buddy sprite

--- main.ts
+++ main.ts
@@ -16,6 +16,25 @@ let mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
+let mySprite2 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . e e e e e e e . . . . 
+. . . . e e e e e e e e e . . . 
+. . . . e e e e e e e e e . . . 
+. . . . e e f e e e f e e . . . 
+. . . . e e e e e e e e e . . . 
+. . . . e e e e e e e e e . . . 
+. . . . e f f e e e f f e . . . 
+. . . . e e f f f f f e e . . . 
+. . . . . e e e e e e e . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+mySprite2.setPosition(100, 60)
 controller.moveSprite(mySprite, 100, 100)
 scene.setBackgroundColor(11)
 mySprite.say("yay", 2000)
